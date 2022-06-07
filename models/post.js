e158d6505ec206783c6e2c37a8e987244ea90a78
Catherine Tung <catt.tung@gmail.com>
2022-06-07 12:59:28 -0400

    Add comments to model and add posts to controllers/routes

--- models/post.js
+++ models/post.js
@@ -2,6 +2,25 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema  = new Schema(
+  {
+    comment_text: {
+      type: String,
+      required: true
+    },
+    codeblock: {
+      type: String,
+      required: false
+    },
+    is_solution: {
+      type:  Boolean,
+      default: false
+    },
+    commenter: {type: mongoose.Schema.Types.ObjectId, ref:'Profile'}
+  },
+  { timestamps: true }
+)
+
 const postSchema = new Schema({
   question: {
     type: String,
@@ -15,10 +34,15 @@ const postSchema = new Schema({
     type: Boolean,
     default: false
   },
-  added_by:  { type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
+  added_by:  { 
+    type: mongoose.Schema.Types.ObjectId, ref: 'Profile'
+  },
+  comments: [commentSchema]
 }, {timestamps: true})
 
 const Post = mongoose.model('Post', postSchema)
+
+
 
 export {
   Post
