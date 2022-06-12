ae9cdef31af94f60058601425cc336ac37137433
Catherine Tung <catt.tung@gmail.com>
2022-06-12 18:31:48 -0400

    Stub up signup component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Nav from '../components/Nav/Nav'
 
 //Services
 
@@ -9,8 +10,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <Nav />
       <Routes>
-        
+
         <Route path='/'
         element={<h1>Landing</h1>}
         />
