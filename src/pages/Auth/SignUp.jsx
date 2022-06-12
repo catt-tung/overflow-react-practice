6c4c0122a0a65012dacd72e3e2474c3f89108cde
Catherine Tung <catt.tung@gmail.com>
2022-06-12 18:36:32 -0400

    Stub up signup

---
+++ src/pages/Auth/SignUp.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../../styles/Auth.css'
+
+//Assets
+import cat from '../../assets/avatars/cat.png'
+
+//Services
+
+const SignUp = (props) => {
+  return (
+    <div className="signup-page">
+
+      <div className='left-container'>
+
+        <div className='form-container'>
+          <div className="title-container">
+            <h1>Create an Account</h1>
+						<h3>Social media for developers</h3>
+          </div>
+          <form className="register-form">
+            Inputs Here
+          </form>
+        </div>
+
+      </div>
+
+      <div className="right-container">
+        Animation Here
+      </div>
+
+    </div>
+  );
+}
+
+export default SignUp;
