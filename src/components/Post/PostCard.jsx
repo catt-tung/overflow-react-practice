ca56ccc3bfe2df0cce644ffb7f7566872e91581a
Catherine Tung <catt.tung@gmail.com>
2022-06-24 12:28:21 -0400

    Add divs to postcard

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const PostCard = (props) => {
   return (
     <div className="post-card">
+      <div className="card-header"></div>
+      <div className="question-container">
+        <p>{props.post.question}</p>
+      </div>
 
+      <div className="code-container">
+        <p>{props.post.codeblock}</p>
+      </div>
+      <div className="comment-link"></div>
     </div>
   );
 }
