7ffc6cbf892c1d54eeb20075d7c65e736bb43c7f
Catherine Tung <catt.tung@gmail.com>
2022-06-24 12:12:53 -0400

    create state for posts

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 //Services
+import { getAllPosts } from "../services/postService";
 
 //Components
 
 const PostList = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      const postData = await getAllPosts()
+      console.log(postData)
+    }
+    fetchAllPosts()
+  }, [])
+
   return (
     <div className="layout">
       <h1>Posts!</h1>
