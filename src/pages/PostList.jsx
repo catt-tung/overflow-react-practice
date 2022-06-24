68d14a8458952da743c53c55a4f427637ad5ccbd
Catherine Tung <catt.tung@gmail.com>
2022-06-24 10:50:11 -0400

    Stub up postlist component

---
+++ src/pages/PostList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Services
+
+//Components
+
+const PostList = () => {
+  return (
+    <div className="layout">
+      <h1>Posts!</h1>
+    </div>
+  );
+}
+
+export default PostList;
