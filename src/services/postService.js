088bf2661d1c529dd1a11105fa2371150b9e322d
Catherine Tung <catt.tung@gmail.com>
2022-06-24 10:54:12 -0400

    fix typo for template literal

--- src/services/postService.js
+++ src/services/postService.js
@@ -3,7 +3,7 @@ const BASE_URL = '/api/posts/'
 
 export const getAllPosts = async () => {
   try {
-    const res = await fetch('${BASE_URL}')
+    const res = await fetch(`${BASE_URL}`)
     const data = await res.json()
     return data
   } catch (error) {
