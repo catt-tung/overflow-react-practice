bb131c1d7a7ec5a3cf34c4a4d2a1107a12b3bee4
Catherine Tung <catt.tung@gmail.com>
2022-06-26 13:41:09 -0400

    Add Header and PostForm to CreatePost

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import '../../styles/Create.css'
 
 //Components
+import PostForm from "./PostForm";
+import Header from '../../components/misc/Header'
 
 const CreatePost = (props) => {
   return (
     <div className="layout">
-
-
+      <Header />
+      <PostForm />
     </div>
   );
 }
