04c582fa978eb3618335eb4d01816c51805f4f8a
Catherine Tung <catt.tung@gmail.com>
2022-07-13 13:14:36 -0400

    Implement service function for updating posts in services

--- src/services/postService.js
+++ src/services/postService.js
@@ -27,3 +27,19 @@ export const createPost = async (post)=> {
     throw error
   }
 }
+
+export const updatePost = async (postId) => {
+  try {
+    const res = await fetch(`${BASE_URL}${postId}`, {
+      method: "PUT",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + tokenService.getToken()
+      },
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
