4f94b3e08871844d1db911550c41be669a41cabc
Catherine Tung <catt.tung@gmail.com>
2022-07-13 13:10:35 -0400

    Post actions component stub up

---
+++ src/components/Post/PostActions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/Card.css'
+
+const PostActions = (props) => {
+  return (
+    <div className='interactions'>
+
+    </div>
+  );
+}
+
+export default PostActions;
