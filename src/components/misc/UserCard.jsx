fb9a13cee5477802ac261e87eebee44bb777edde
Catherine Tung <catt.tung@gmail.com>
2022-07-29 23:20:36 -0400

    Stub up UserCard component

---
+++ src/components/misc/UserCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../styles/UserCard.css'
+
+const UserCard = (props) => {
+  return (
+    <div className="user-card">
+      <img alt="user animal avatar"/>
+      <div className="user-info">
+        <h4>hi</h4>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
